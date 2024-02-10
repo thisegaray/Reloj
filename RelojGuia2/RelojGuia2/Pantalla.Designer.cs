4dd4fbac47648d17f0827ecbe249dcb716188451
E. Garay <96841421+thisegaray@users.noreply.github.com>
2024-02-10 00:47:31 -0600

    Add files via upload

---
+++ RelojGuia2/RelojGuia2/Pantalla.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace RelojGuia2
+{
+    partial class Pantalla
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelReloj = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // PanelReloj
+            // 
+            this.PanelReloj.Location = new System.Drawing.Point(12, 12);
+            this.PanelReloj.Name = "PanelReloj";
+            this.PanelReloj.Size = new System.Drawing.Size(760, 437);
+            this.PanelReloj.TabIndex = 0;
+            this.PanelReloj.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelReloj_Paint);
+            // 
+            // Pantalla
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.PanelReloj);
+            this.MaximizeBox = false;
+            this.Name = "Pantalla";
+            this.Text = "Reloj";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelReloj;
+    }
+}
+
---
+++ RelojGuia2/RelojGuia2/Pantalla.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RelojGuia2
+{
+    public partial class Pantalla : Form
+    {
+        public class Reloj
+        {
+            // Variables locales
+            private int hora = 12;
+            private int minutos = 0;
+            private string meridiano = "";
+            private Graphics lienzo;
+
+            // Enums
+            private enum Numeros
+            {
+                CERO = 0,
+                UNO = 1,
+                DOS = 2,
+                TRES = 3,
+                CUATRO = 4,
+                CINCO = 5,
+                SEIS = 6,
+                SIETE = 7,
+                OCHO = 8,
+                NUEVE = 9
+            }
+
+            // Métodos Constructores
+            public Reloj() { }
+            public Reloj(int Hora24, int Min)
+            {
+                // MessageBox.Show($"Son las {Hora24}:{Min}");
+            }
+
+            // Métodos privados
+            private string ComprobarTiempo(int Hora24)
+            {
+                if (Hora24 > 12)
+                {
+                    return "pm";
+                }
+                else if (Hora24 <= 12)
+                {
+                    return "am";
+                }
+                else
+                {
+                    MessageBox.Show("Hora no válida");
+                    return "";
+                }
+            }
+
+            public void DibujarNumeros(Graphics lienzo, Point Ubicacion, int Hora24, int Min)
+            {
+                switch(Hora24)
+                {
+                    case 1:
+                        {
+                            Size tamanio = new Size(10, 50);
+                            Brush pincel = new SolidBrush(Color.Red);
+                            Rectangle rectangulo = new Rectangle(Ubicacion, tamanio);
+                            lienzo.FillRectangle(pincel, rectangulo);
+                        }
+                        break;
+                    case 2:
+                        {
+                            MessageBox.Show($"{(int) Numeros.DOS}");
+                        }
+                        break;
+                    case (int)Numeros.TRES:
+                        {
+                            MessageBox.Show($"{(int)Numeros.TRES}");
+                        }
+                            break;
+                    case (int)Numeros.CUATRO:
+                        {
+                            MessageBox.Show($"{(int)Numeros.CUATRO}");
+                        }
+                            break;
+                    case (int)Numeros.CINCO:
+                        {
+                            MessageBox.Show($"{(int)Numeros.CINCO}");
+                        }
+                            break;
+                    case (int)Numeros.SEIS:
+                        {
+                            MessageBox.Show($"{(int)Numeros.DOS}");
+                        }
+                            break;
+                    case (int)Numeros.SIETE:
+                        {
+                            MessageBox.Show($"{(int)Numeros.SIETE}");
+                        }
+                            break;
+                    case (int)Numeros.OCHO:
+                        {
+                            MessageBox.Show($"{(int)Numeros.OCHO}");
+                        }
+                            break;
+                    case (int)Numeros.NUEVE:
+                        {
+                            MessageBox.Show($"{(int)Numeros.DOS}");
+                        }
+                            break;
+                }
+            }
+
+            // Métodos públicos
+            public void AsignarHora(int Hora24, int Min)
+            {
+                hora = Hora24;
+                minutos = Min;
+                meridiano = ComprobarTiempo(Hora24);
+                MessageBox.Show($"{Hora24}:{Min} {meridiano}");
+            }
+            public void VerEnPanel(Panel grafico, Point Ubicacion) //Point ubicacion)
+            {
+                lienzo = grafico.CreateGraphics();
+                DibujarNumeros(lienzo, Ubicacion, hora, minutos);
+            }
+        }
+
+        public Pantalla()
+        {
+            InitializeComponent();
+        }
+
+        private void PanelReloj_Paint(object sender, PaintEventArgs e)
+        {
+            // Instancias
+            //Reloj reloj1 = new Reloj(10, 10);
+
+            Reloj reloj2 = new Reloj();
+            reloj2.AsignarHora(2, 20);
+            reloj2.VerEnPanel(PanelReloj, new Point(20,20));
+
+            Reloj reloj3 = new Reloj();
+            reloj3.AsignarHora(1, 59);
+            reloj2.VerEnPanel(PanelReloj, new Point(30, 30));
+        }
+    }
+}
